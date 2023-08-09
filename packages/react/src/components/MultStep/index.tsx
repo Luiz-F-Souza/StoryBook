5a94c7f1cc28bda65e67fc4541693d6d33baf7a2
Luiz Felipe Souza <luizfelipesouza1998@outlook.com>
2023-08-09 19:49:12 -0300

    Multstep component and story created

---
+++ packages/react/src/components/MultStep/index.tsx
@@ -0,0 +1,21 @@
+import { Label, MultStepContainer, Step, Steps } from "./styles"
+
+
+export interface MultStepProps {
+  size: number,
+  currentStep?: number
+}
+
+export const MultStep = ({size, currentStep = 1}: MultStepProps) => {
+
+  return(
+    <MultStepContainer>
+      <Label>Passo {currentStep} de {size}</Label>
+      <Steps css={{"--steps-size":size}}>
+        {Array.from({length: 4}, (_, i) => i + 1).map(step => {
+          return <Step key={step} active={currentStep >= step}/>
+        })}
+      </Steps>
+    </MultStepContainer>
+  )
+}
